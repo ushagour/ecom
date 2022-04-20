f2eccd8760710a4197f8ba7467887f17e1cf0e03
ali <ali.ouchagour01@gmail.com>
2022-04-20 16:05:02 +0200

    creating middelware authamin for user and admin

--- app/Http/Middleware/AuthAdmin.php
+++ app/Http/Middleware/AuthAdmin.php
@@ -16,6 +16,17 @@ class AuthAdmin
      */
     public function handle(Request $request, Closure $next)
     {
+
+        if (session('utype')==='ADM') {
+            
+            return $next($request);
+        }
+        else{
+            session()->flush();
+            return redirect()->route('login');
+            
+
+        }
         return $next($request);
     }
 }
