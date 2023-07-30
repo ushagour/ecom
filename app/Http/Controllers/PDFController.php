668d8a967f0d40ca80f4c052c1601d8cfda5fa68
ali ouchagour <ali.ouchagour01@gmail.com>
2023-07-30 13:41:12 +0100

    generate pdf

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use PDF;
+use App\Models\Order;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
   
 class PDFController extends Controller
 {
@@ -14,8 +17,30 @@ class PDFController extends Controller
      */
     public function generateInvoicePDF()
     {
-        $pdf = PDF::loadView('vendor.invoices.templates.myPDF');
-        return $pdf->download('nicesnippets.pdf');
+       
+
+        // $customer = new Buyer([
+        //     'name'          => 'John Doe',
+        //     'custom_fields' => [
+        //         'email' => 'test@example.com',
+        //     ],
+        // ]);
+
+        // $item = (new InvoiceItem())->title('Service 1')->pricePerUnit(2);
+
+        // $invoice = Invoice::make()
+        //     ->buyer($customer)
+        //     ->discountByPercent(10)
+        //     ->taxRate(15)
+        //     ->shipping(1.99)
+        //     ->addItem($item);
+
+        // return $invoice->stream();
+        $order = Order::all();
+
+           dd($order->shipping->id);
+        // $pdf = PDF::loadView('vendor.invoices.templates.invoice_order_receipt', array('order'=>$order));
+        // return $pdf->download('nicesnippets.pdf');
 
     }
 }
